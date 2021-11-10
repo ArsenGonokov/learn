abf2864aab2bc082af44bd47f3a42c1a90877a9b
Arsen Gonokov <vendetta.fox@gmail.com>
2021-11-10 22:41:05 +0300

    Co-authored-by: Aslan Sosmakov <aslansosmakov@gmail.com>

--- alert.js
+++ alert.js
@@ -1,7 +1,32 @@
 'use strict';
-let value = prompt ('Какое официальное название JavaScript?', '')
-if (value == 'ECMAScript') {
-    alert ('Верно!');
-} else {
-    alert ('Не знаете? ECMAScript!')
+
+
+function calc (math, a, b){
+    if (math == 'sum') {
+        console.log (a + b);
+    }
+    if (math == 'sub') {
+        console.log (a-b);
+    }
+    if (math == 'div') {
+        if (b == 0){
+            console.log ('На ноль делить нельзя сука!')
+        } else {
+            console.log (a/b);
+        }
+        
+    }
+    if (math == 'multi') {
+        console.log (a*b);
+    }
 }
+
+// calc('sum', 3, 3);
+// calc('sub', 3, 2);
+// calc('div', 6, 2);
+// calc('multi', 2, 2);
+calc('div', 2, 0);
+
+// написать программу которая проверят что ты совершеннолетний
+// написать программу которая проверят время года
+// написать программу которая проверят рабочие часы
